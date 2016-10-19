29bd6fcee64fbc7d78b7a55a0a6988c9da328e96
Simon B-J <simon_bj@live.com>
2016-10-19 16:32:58 +0000

    Removed shit code

--- play-simple-sound/main.js
+++ play-simple-sound/main.js
@@ -3,7 +3,3 @@ var player = require('play-sound')(opts = {})
 player.play('../resources/klassefesten-intro.mp3', function(err){
   if (err) throw err;
 });
-
-player.play('../resources/klassefesten-intro.mp3', { timeout: 300 }, function(err){
-  if (err) throw err;
-});
