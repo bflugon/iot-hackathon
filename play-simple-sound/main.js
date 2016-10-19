14d56b7b74fca6bce28ee9115324b036f4e7d403
Bård S. Flugon <bflugon@gmail.com>
2016-10-19 19:02:47 +0200

    Added async while

--- play-simple-sound/main.js
+++ play-simple-sound/main.js
@@ -1,5 +1,7 @@
 var player = require('play-sound')(opts = {})
+var async = require('async')
 
+async.whilst(true) {
 player.play('../resources/klassefesten-intro.mp3', function(err){
   if (err) throw err;
-});
+});}
